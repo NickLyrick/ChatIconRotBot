806f55ea54899867b1ed246f8828f87b192ed1ac
Alexander Kirilkin <a.kirilkin@sportradar.com>
2024-02-12 00:15:34 +0100

    add get_top handler

--- src/handlers/chat_handlers.py
+++ src/handlers/chat_handlers.py
@@ -1,7 +1,8 @@
 from aiogram import Bot, Router, types
-from aiogram.filters import Command
-
+from aiogram.filters import Command, CommandObject
+from datetime import datetime
 from src.database.connect import Request
+
 from src.utility.tools import table
 
 chat_router = Router()
@@ -21,3 +22,36 @@ async def show_queue(message: types.Message, bot: Bot, request: Request) -> None
         media = table(data, ["Nickname", "Game", "Platform"])
         media[0].caption = "Очередь трофеев"
         await bot.send_media_group(chat_id=chat_id, media=media)
+
+
+@chat_router.message(Command("top"))
+async def top(message: types.Message, bot: Bot, command: CommandObject, request: Request) -> None:
+    # TODO: decide what to do with users
+    # await request.add_user_data(message.from_user.id, message.from_user.username)
+    chat_id = message.chat.id
+
+    arguments = command.args
+    date = datetime(1980, 1, 1)
+
+    caption = "Топ за всё время"
+    if arguments is not None:
+        try:
+            date = datetime.strptime(arguments, '%d.%m.%Y')
+            caption = f"Топ от {arguments}"
+        except ValueError:
+            caption = "Топ за всё время"
+
+    data = []
+    try:
+        data = await request.get_top(chat_id=chat_id, date=date)
+        print(data)
+    except Exception as e:
+        await message.reply(text=f"Не удалось получить топ от {date}"
+                                 f"Ошибка: {e}")
+
+    if len(data) > 0:
+        media = table(data, ["Nickname", "Trophies"])
+        media[0].caption = caption
+        await bot.send_media_group(chat_id=chat_id, media=media)
+    else:
+        await message.reply("Список пуст!")
